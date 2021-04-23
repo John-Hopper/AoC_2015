1f553317832f577bcd4247fd5280a3067d6b61b4
John Hopper <JohnHopper.Dev@outlook.com>
2021-04-23 10:58:38 +0100

    Start of Day_03 code and alignment of frameworks used for coding

---
+++ Day_03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Utility_Library;
+
+namespace Day_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize Global Variables & get variable info
+            Globals globalVariables = new Globals();
+            globalVariables = globalVariables.GetGlobalVariables();
+
+            // check environment and global variables initialized ok
+            if (globalVariables.Failed)
+            {
+                Console.WriteLine($"Environment not found - Initialization Failed");
+                Console.ReadLine();
+                return;
+            }
+
+            //read data file
+            var dimensionsData = DataRepository.ReadToString($"{globalVariables.DataPath}151203 Input.txt");
+
+            Console.WriteLine(dimensionsData);
+            Console.ReadLine();
+        }
+    }
+}
