873a462b6e37a8a3754ac78c7ea0c4349052b4d6
John Hopper <JohnHopper.Dev@outlook.com>
2021-05-01 21:34:07 +0100

    Day_04 completed

---
+++ Day_04/Crypto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Day_04
+{
+    public static class Crypto
+    {
+        public static string GetHash(string input)
+        {
+            var returnHash = "";
+
+            var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(input.ToCharArray()));
+            foreach (byte h in hash)
+            {
+                returnHash += (h.ToString("x2"));
+            }
+            return returnHash;
+        }
+
+        public static int GetSecretNumber(string key, int leadingZeros)
+        {
+            string firstFiveCheck = "";
+            for(int index=0; index<leadingZeros; index++)
+            {
+                firstFiveCheck += "0";
+            }
+            var keyNumber = 1;
+            var secretNumber=GetHash(key + keyNumber.ToString());
+            string firstFivChar = new string(secretNumber.Take(leadingZeros).ToArray());
+
+            while (firstFivChar != firstFiveCheck)
+            {
+                keyNumber++;
+                secretNumber = GetHash(key + keyNumber.ToString());
+                firstFivChar = new string(secretNumber.Take(leadingZeros).ToArray());
+            } 
+
+            return keyNumber;
+        }
+    }
+}
---
+++ Day_04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Day_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            var key = "bgvyzdsv";
+
+            //PART ONE
+            Console.WriteLine("---PART ONE---\n\r");
+            stopwatch.Start();
+
+            var secretNumber = Crypto.GetSecretNumber(key, 5);
+
+            stopwatch.Stop();
+            Console.WriteLine($"secret number with giving five zeros: {secretNumber}");
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.ReadLine();
+
+            //PART TWO
+            Console.WriteLine("---PART TWO---\n\r");
+            stopwatch.Start();
+
+            secretNumber = Crypto.GetSecretNumber(key, 6);
+
+            stopwatch.Stop();
+            Console.WriteLine($"secret number with giving six zeros: {secretNumber}");
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.ReadLine();
+        }
+    }
+}
