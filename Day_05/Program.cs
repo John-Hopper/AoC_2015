c6525a1a3e61a366a09de55fdedc54955248065d
John Hopper <JohnHopper.Dev@outlook.com>
2021-05-03 21:38:28 +0100

    Day_05 Part one complete

---
+++ Day_05/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Utility_Library;
+using static Day_05.ValidTests;
+
+namespace Day_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            //read data file
+            var data = DataRepository.ReadToArray("151205 Input.txt");
+
+            //PART ONE
+
+            Console.WriteLine("---PART ONE---\n\r");
+            stopwatch.Start();
+
+            var niceCount = 0;
+
+            foreach (string line in data)
+            {
+                if (!ContainsDisallowedLetterSet(line)) 
+                {
+                    if (ContainsVowel(line))
+                    {
+                        if(ContainsDoubleLetter(line)) niceCount++;
+                    }
+                }
+            }           
+
+            stopwatch.Stop();
+            Console.WriteLine($"secret number with giving five zeros: {niceCount}");
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.ReadLine();
+
+            //PART TWO
+            Console.WriteLine("---PART TWO---\n\r");
+            stopwatch.Start();
+
+            
+
+            stopwatch.Stop();
+            Console.WriteLine($"secret number with giving six zeros: {1}");
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day_05/ValidTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+
+namespace Day_05
+{
+    public static class ValidTests
+    {
+        public static bool ContainsVowel(string testString)
+        {
+            var vowelCount = 0;
+            char[] vowels = "aeiou".ToCharArray();
+
+            foreach(char vowel in vowels)
+            {
+                vowelCount += testString.Count(f => (f == vowel));
+            }
+
+            if (vowelCount >= 3) return true;
+            return false;
+        }
+
+        public static bool ContainsDoubleLetter(string testString)
+        {
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            foreach(char letter in alphabet)
+            {
+                var doubleLetter = letter.ToString() + letter.ToString();
+                if (testString.Contains(doubleLetter)) return true;
+            }
+            return false;
+        }
+
+        public static bool ContainsDisallowedLetterSet(string testString)
+        {
+            string[] disallowedLetterSet = new string[] { "ab", "cd", "pq", "xy" };
+
+            foreach (string letterSet in disallowedLetterSet)
+            {
+                if (testString.Contains(letterSet)) return true;
+            }
+            return false;
+        }
+    }
+}
